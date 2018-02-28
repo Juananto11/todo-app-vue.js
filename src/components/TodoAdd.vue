3206f8347e005eb1a4d7b0d243e12cd495428ba1
Juananto11 <juananto11@hotmail.com>
2018-02-27 18:08:41 -0600

    Se agregon contadores de tareas

--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -4,6 +4,10 @@
     h2(v-else) Crear una tarea
     input(type="text" name="text" placeholder="Ingresa una tarea" v-model="newTask" @keyup.enter="addTask")
     button(@click="addTask") Agregar tarea
+    .dataTask(v-if="list.length > 0")
+      small.tareas Tareas: {{ list.length }}
+      small.completadas Completadas: {{ completadas }}
+      small.pendientes Pendientes: {{ pendientes }}
 </template>
 
 <script>
@@ -26,6 +30,14 @@ export default {
         })
       }
       this.newTask = ''
+    }
+  },
+  computed: {
+    completadas () {
+      return this.list.map(el => el.completed).filter(el => el === true).length
+    },
+    pendientes () {
+      return this.list.map(el => el.completed).filter(el => el === false).length
     }
   }
 }
